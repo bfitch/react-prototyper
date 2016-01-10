c21fc82c5ec3577d9763c0a8ae3afe2843824670
Brian Fitch <brianmfitch@gmail.com>
2016-01-09 21:06:26 -0500

    initial commit

---
+++ test/main-test.js
@@ -0,0 +1,21 @@
+var mocha          = require('mocha');
+var chai           = require('chai');
+var sinon          = require('sinon');
+var sinonChai      = require('sinon-chai');
+chai.use(sinonChai);
+
+var describe = mocha.describe;
+var it       = mocha.it;
+var expect   = chai.expect;
+
+describe('', () => {
+  beforeEach(() => {
+  });
+
+  describe('', () => {
+    it ('', () => {
+
+      expect().to.not.have.been.called
+    });
+  });
+});
